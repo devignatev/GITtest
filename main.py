b2a4bf4183d620deb2041a228c83dad1d8c3738e
devignatev <Levosllavny@gmail.com>
2024-03-08 15:38:29 +0300

    C2

--- main.py
+++ main.py
@@ -14,3 +14,36 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+import tkinter as tk
+
+def calculate_interest():
+    deposit_amount = float(entry.get())
+    if deposit_amount < 100:
+        interest_rate = 0.05
+    elif 100 <= deposit_amount <= 200:
+        interest_rate = 0.07
+    else:
+        interest_rate = 0.10
+
+    total_amount = deposit_amount + (deposit_amount * interest_rate)
+    result_label.config(text=f"Общая сумма с начисленными процентами: {total_amount}")
+
+
+# Создание графического интерфейса
+root = tk.Tk()
+root.title("Расчет процентов по вкладу")
+
+label = tk.Label(root, text="Введите сумму вклада:")
+label.pack(pady=10)
+
+entry = tk.Entry(root)
+entry.pack()
+
+calculate_button = tk.Button(root, text="Рассчитать", command=calculate_interest)
+calculate_button.pack(pady=5)
+
+result_label = tk.Label(root, text="")
+result_label.pack(pady=10)
+
+root.mainloop()
